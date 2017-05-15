24306af3b41007ef599457617d37c5c7add36cc5
Lauren Wolfe <wolfela@uw.edu>
2017-05-15 01:53:03 -0700

    postgres

--- node-postgres-promises/routes/index.js
+++ node-postgres-promises/routes/index.js
@@ -3,18 +3,22 @@ var router = express.Router();
 var pg = require('pg');
 
 pg.defaults.ssl = true;
-app.get('/db', function(request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client.query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) { console.log(JSON.stringify(row)); });
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/db', function (request, response, next) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
 });
 
 module.exports = router;
