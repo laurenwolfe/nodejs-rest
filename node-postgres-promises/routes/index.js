410903162a3b4060b205070fdef5500f12e900a2
Lauren Wolfe <wolfela@uw.edu>
2017-05-15 02:33:45 -0700

    postgres

--- node-postgres-promises/routes/index.js
+++ node-postgres-promises/routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
+var pg = require('pg').native
+    , connectionString process.env.DATABASE_URL || 'postgres://localhost:5432/spd_db'
+    , client
+    , query;
 
 pg.defaults.ssl = true;
+
+client = new pg.Client(connectionString);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,14 +15,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/db', function (request, response, next) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
-            done();
-            if (err)
-            { console.error(err); response.send("Error " + err); }
-            else
-            { response.render('pages/db', {results: result.rows} ); }
-        });
+    client.connect();
+
+    client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err) {
+            console.error(err); response.send("Error " + err);
+        } else {
+            response.render('pages/db', {results: result.rows});
+        }
     });
 });
 
